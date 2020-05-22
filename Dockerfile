2d1dd88c0a19f53483046497d9d0701c27cd344d
Mikhail Grachev <work@mgrachev.com>
2020-05-22 19:19:37 +0300

    Update reviewdog
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM ruby:2.7-alpine
 
-ENV REVIEWDOG_VERSION v0.9.17
+ENV REVIEWDOG_VERSION v0.10.0
 
 SHELL ["/bin/ash", "-eo", "pipefail", "-c"]
 RUN apk add --update --no-cache build-base git
 RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /usr/local/bin/ $REVIEWDOG_VERSION
-RUN gem install -N brakeman
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-cd "$GITHUB_WORKSPACE"
+version() {
+  if [ -n "$1" ]; then
+    echo "-v $1"
+  fi
+}
 
+cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+gem install -N brakeman $(version $INPUT_BRAKEMAN_VERSION)
+
 brakeman --quiet --format tabs ${INPUT_BRAKEMAN_FLAGS} \
-  | reviewdog -f=brakeman -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -f=brakeman \
+    -name="${INPUT_TOOL_NAME}" \
+    -reporter="${INPUT_REPORTER}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS}
